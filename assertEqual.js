f75f660d8cb5afc09a7aea45d022e47e3da5571f
Bill Watters <billywatters13@gmail.com>
2020-09-14 21:57:47 +0000

    Update to readability of message

--- assertEqual.js
+++ assertEqual.js
@@ -1,5 +1,5 @@
 const assertEqual = function(actual, expected) {
-  const pass = `✅✅✅Assertion Passed: ${actual} === ${expected}\n`; 
+  const pass = `Assertion Passed: ✅✅✅ ${actual} === ${expected}\n`; 
   const fail = `🔴🔴🔴 ${actual} !== ${expected}\n`;
   return console.assert(actual === expected, fail, pass);
 
