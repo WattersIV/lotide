97aa8ee24a16cf01c742bd585cc306b1d326f730
Bill Watters <billywatters13@gmail.com>
2020-09-16 19:30:21 +0000

    Added function to output letter positions in an object

---
+++ letterPositions.js
@@ -0,0 +1,58 @@
+const assertArraysEqual = function (arr1, arr2) {
+  let equalLen = false; 
+  const notSame = 'These are not equal'; 
+  const same = 'These are equal';
+  if (arr1.length == arr2.length) {
+    equalLen = true; 
+  } 
+  if (equalLen) {  
+    for (let i = 0; i < arr1.length; i++){
+      if (arr1[i] !== arr2[i]) {  
+        console.log(notSame); 
+        return 0;
+      } 
+    } 
+    console.log(same); 
+    return 1;
+  } 
+  console.log(notSame); 
+  return 0;
+} 
+const eqArrays = function (arr1, arr2) {
+  let equalLen = false;
+  if (arr1.length == arr2.length) {
+    equalLen = true; 
+  } 
+  if (equalLen) { 
+    let equal = false;
+    for (let i = 0; i < arr1.length; i++){
+      if (arr1[i] == arr2[i]) {  
+        equal = true;
+      } else {
+        equal = false; 
+        return equal;
+      }  
+    } 
+    return equal;
+  } 
+  return false;
+}
+
+const letterPositions = function(sentence) {
+  const results = {};
+  let i = 0;
+  for (const index of sentence) { 
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i); 
+      i++;
+    } else {
+      results[sentence[i]] = []; 
+      results[sentence[i]].push(i); 
+      i++;
+    }
+  }
+  return results;
+}; 
+
+console.log(letterPositions('Hello'))
+assertArraysEqual(letterPositions('hello').e, [1]);
