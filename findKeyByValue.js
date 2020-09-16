4e32e58d85085af90e2af01fd5f25328800cf008
Bill Watters <billywatters13@gmail.com>
2020-09-16 19:52:44 +0000

    Added function to find a key by using its value as an argument

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  const pass = `Assertion Passed: ✅✅✅ ${actual} === ${expected}\n`; 
+  const fail = `🔴🔴🔴 ${actual} !== ${expected}\n`;
+  if (actual === expected){
+    console.log(pass);
+  } else {
+    console.assert(actual === expected, fail); 
+  }
+}  
+
+const findKeyByValue = function (source, value) {
+  const keys = Object.keys(source);  
+  let i = 0;
+  for (const item of keys) { 
+    i++;
+    if (source[keys[i]] === value) {
+      return keys[i];
+    }
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
