43a3952850faba77325e6f600633cdf7e8b99dc2
Bill Watters <billywatters13@gmail.com>
2020-09-17 20:29:34 +0000

    Added function to output first key in an object which returns true in the function passed as an argument

---
+++ findKey.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+  const pass = `Assertion Passed: ✅✅✅ ${actual} === ${expected}\n`; 
+  const fail = `🔴🔴🔴 ${actual} !== ${expected}\n`;
+  if (actual === expected){
+    console.log(pass);
+  } else {
+    console.assert(actual === expected, fail); 
+  }
+}; 
+
+const findKey = (object, callback) => {
+  let output = '';
+  const keys = Object.keys(object); 
+for (let i = 0; i < keys.length; i++) { 
+    if (callback(object[keys[i]])){
+      output+= keys[i];  
+      console.log(output);
+      return output;
+    }
+  }  
+}
+
+//   for (prop in object) {
+//     if (callback(prop[i])) {
+//       output += prop[i];
+//     }
+//   } 
+// }
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3) // => "noma"
