793d72b81cdb1164df70ab374d97e50352cb9e0e
Bill Watters <billywatters13@gmail.com>
2020-09-17 19:07:51 +0000

    Added map function to take an array and modify it with a callback

---
+++ map.js
@@ -0,0 +1,33 @@
+const map = (array, callback) => {
+  const output = [];
+  array.forEach(element => {
+    output.push(callback(element)); 
+  });
+  return output;
+} 
+
+const assertArraysEqual = function (arr1, arr2) {
+  let equalLen = false; 
+  const notSame = 'These are not equal'; 
+  const same = 'These are equal';
+  if (arr1.length == arr2.length) {
+    equalLen = true; 
+  } 
+  if (equalLen) {  
+    for (let i = 0; i < arr1.length; i++){
+      if (arr1[i] !== arr2[i]) {  
+        console.log(notSame); 
+        return 0;
+      } 
+    } 
+    console.log(same); 
+    return 1;
+  } 
+  console.log(notSame); 
+  return 0;
+}
+
+const words = ["g", "c", "t", "m", "t"];
+results1 = assertArraysEqual(map(words, word => word[0]), words);
+console.log(results1);  
+
